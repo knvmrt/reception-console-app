165d6279d4289ca81f4fdce02c279b8ed5ab19d5
knvmrt <knvmrt@gmail.com>
2024-04-26 12:31:05 +0400

    Reception Console App

---
+++ ReceptionConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("First name:");
+        string FirstName = Console.ReadLine();
+
+        Console.WriteLine("Last name:");
+        string LastName = Console.ReadLine();
+
+        Console.WriteLine("Your age:");
+        int Age = Convert.ToInt32(Console.ReadLine());
+
+        //Type your own file path.
+        string fileLocation = "D:\\Visual Studio Projects\\ReceptionConsoleApp\\ReceptionConsoleApp\\Logs.txt";
+
+        using (StreamWriter writer = new StreamWriter(fileLocation, true))
+        {
+            writer.WriteLine($"FirstName: {FirstName}  LastName: {LastName}  Age: {Age}");
+        }
+
+        Console.WriteLine("Datas saved to file.");
+        Console.ReadLine();
+    }
+}
